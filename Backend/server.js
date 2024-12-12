3ee9b62149ac7f4c8cff7780a0ac3f2eb0a26970
Muhammad-Farasat <farasatkhan687@gmail.com>
2024-12-12 23:59:41 +0500

    change-of-dir

--- Backend/server.js
+++ Backend/server.js
@@ -43,9 +43,9 @@ app.get('/test', (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, "/Frontend/dist")))
+app.use(express.static(path.join(__dirname, "/dist")))
 app.get("*", (req, res)=>{
-  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"))
+  res.sendFile(path.join(__dirname,  "dist", "index.html"))
 })
 
 server.listen(PORT, ()=> {
