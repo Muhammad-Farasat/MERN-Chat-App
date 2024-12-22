284a47adf9a5204b3b7bdb2e1178c6d06ffc6d98
Muhammad-Farasat <farasatkhan687@gmail.com>
2024-12-22 17:07:11 +0500

    login-uncommented

--- Frontend/src/Hooks/useLogin.js
+++ Frontend/src/Hooks/useLogin.js
@@ -17,7 +17,7 @@ const useLogin = () => {
             
             const res = await fetch(`${backend_url}/auth/login`, {
                 method: "POST",
-                // headers: { "content-type": "application/json"},
+                headers: { "content-type": "application/json"},
                 body: JSON.stringify({username, password}),
             });
 
