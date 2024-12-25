ca8d1311cad5bfce910bab84691d851331460844
Muhammad-Farasat <farasatkhan687@gmail.com>
2024-12-25 21:37:13 +0500

    socket

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || '5000'
 
 dotenv.config();  
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://chat-app-frontend-plum.vercel.app',
+  methods: ["GET", "POST"]
+}))
 
 app.use(express.json());
 
